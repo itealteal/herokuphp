dcdbbde954222af5e1d7c024106067e9a8c3d9bd
itealteal <20033824@myrp.edu.sg>
2022-07-04 14:39:12 +0800

    second

--- web/test.php
+++ web/test.php
@@ -1,5 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Put your description here.">
+    <title>Test</title>
 </head>
 <body>
   Hello World!
